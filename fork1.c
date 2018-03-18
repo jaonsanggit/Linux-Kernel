67bbdf6660f70dea9f2b2cbf64ab9f42578d9708
桑宏锐 <sandro@sanghongruideMacBook-Pro.local>
2018-03-18 20:19:14 +0800

    进程复制，父进程wait子进程退出之后再退出，父进程可以获得子进程退出状态

--- fork1.c
+++ fork1.c
@@ -1,4 +1,5 @@
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,6 +9,7 @@ int main(int argc, char const *argv[])
 	pid_t pid;
 	char *message;
 	int n;
+	int exit_code;
 	printf("fork program starting...\n");
 	pid = fork();
 	switch(pid)
@@ -18,10 +20,12 @@ int main(int argc, char const *argv[])
 		case 0:
 			message = "This is child.";
 			n = 5;
+			exit_code = 27;
 			break;
 		default:
 			message = "This is parent.";
 			n = 3;
+			exit_code = 0;
 			break;
 	}
 	for (; n>0; n--)
@@ -29,6 +33,19 @@ int main(int argc, char const *argv[])
 		puts(message);
 		sleep(1);
 	}
-	exit(0);
+	if (pid != 0)	//parent
+	{		
+		int stat_val;
+		pid_t child_pid;
+
+		child_pid = wait(&stat_val);
+
+		printf("Child progrm has finished: PID = %d\n", child_pid);
+		if (WIFEXITED(stat_val))
+			printf("Child exited with exit_code %d\n", WEXITSTATUS(stat_val));
+		else 
+			printf("Child terminated abnormally\n");
+	}
+	exit(exit_code);
 	return 0;
 }
